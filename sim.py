9f768fa95b99a2864be4fedbaade577a99a0f997
Eddie Reinhardt <rothreinhardts@gmail.com>
2023-10-18 00:07:26 -0500

    Update sim.py

--- sim.py
+++ sim.py
@@ -522,55 +522,9 @@ def play_game(home,away):
                 yardline = 25
                 break
     # print("FINAL SCORE\n",home," ",home_score,"\n",away," ",away_score,"\n")
-    return home_score,away_score,home_pass_yards,home_run_yards,away_pass_yards,away_run_yards
-
-def play_many_games(num,spread,home,away,ou):
-    num_wins_home = 0
-    num_wins_away = 0
-    home_covers = 0
-    pushes = 0
-    overs = 0
-    hpy = 0
-    hry = 0
-    apy = 0
-    ary = 0
-    scores = []
-    yards_gained = []
-    for x in range(num):
-        h_score, a_score, hpy, hry, apy, ary = play_game(home,away)
-        if(h_score > a_score):
-            num_wins_home += 1
+    if(home_score == away_score):
+        if(random.random() >= 0.5):
+            home_score += 3
         else:
-            num_wins_away += 1
-        if(h_score + spread > a_score):
-            home_covers += 1
-        elif(h_score + spread == a_score):
-            pushes += 1
-        if(h_score + a_score > ou):
-            overs += 1
-        scores.append([h_score,a_score])
-        yards_gained.append([hpy,hry,apy,ary])
-    print("\nRESULTS:")
-    print("Home team wins:", num_wins_home)
-    print("Away team wins:", num_wins_away)
-    print("Home team covers:", home_covers)
-    if(pushes != 0):
-        print("With",pushes,"pushes")
-    print("Overs hit:",overs)
-    count = False
-    print("\nOPINION OF THIS SIM:")
-    if(home_covers >= num*0.65):
-        print("\nBet Home Team Covering")
-    elif(home_covers <= num*0.35):
-        print("\nBet Away Team Covering")
-    else:
-        count = True
-    if(overs >= num*0.65):
-        print("\nBet Over")
-    elif(overs <= num*0.35):
-        print("\nBet Under")
-    elif(count):
-        print("\nStay away from this game")
-    print()
-    #print(scores)
-    # print(yards_gained)
+            away_score += 3
+    return home_score,away_score,home_pass_yards,home_run_yards,away_pass_yards,away_run_yards
